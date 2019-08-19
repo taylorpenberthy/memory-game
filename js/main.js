5920998ca45da54a82153ef10b93309e44acda81
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-08-19 12:46:45 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}];
 var cardsInPlay = [];
 
 function CheckForMatch() {
@@ -21,8 +41,16 @@ function flipCard(cardId) {
 		alert("Sorry, try again.");
 		}
 	}
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 flipCard(0);
 flipCard(2);
+
+function createBoard(){
+	for (var i = 0; i < cardsInPlay.length; i++) {
+		var cardElement = document.createElement('img');
+	}
+}
